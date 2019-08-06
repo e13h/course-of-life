66bb8dc4bff00ce91eb701f700cdd48172d91dae
Evan Smith <evanphilipsmith@gmail.com>
2019-08-06 11:07:41 -0600

    route the homepage to resume component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ResumeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
