64c974917c7014d17051d84fbbbd93d35f973d27
Evan Smith <evanphilipsmith@gmail.com>
2019-08-06 14:21:40 -0600

    add material list elements to shared module

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCheckboxModule,
     MatCardModule,
+    MatListModule,
+    MatDividerModule,
   ],
   exports: [
     MatButtonModule,
     MatCheckboxModule,
     MatCardModule,
+    MatListModule,
+    MatDividerModule,
   ]
 })
 export class SharedModule { }
