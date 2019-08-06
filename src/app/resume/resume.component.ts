d0cdfd596af93a8b627f8c0b872c2f5ce9708640
Evan Smith <evanphilipsmith@gmail.com>
2019-08-06 17:00:23 -0600

    add functionality to add experience dialog

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkExperienceComponent } from '../work-experience/work-experience.component';
 import { WorkExperiencesService } from '../services/work-experiences.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddExperienceDialogComponent } from '../add-experience-dialog/add-experience-dialog.component';
 
 @Component({
   selector: 'app-resume',
@@ -10,7 +12,7 @@ import { WorkExperiencesService } from '../services/work-experiences.service';
 export class ResumeComponent implements OnInit {
   private _workExperiences: WorkExperienceComponent[];
 
-  constructor(_service: WorkExperiencesService) {
+  constructor(_service: WorkExperiencesService, public dialog: MatDialog) {
     this._workExperiences = _service.getExperiences();
   }
 
@@ -19,4 +21,13 @@ export class ResumeComponent implements OnInit {
 
   get workExperiences() { return this._workExperiences; }
 
+  openDialogAddExperience(): void {
+    const dialogRef = this.dialog.open(AddExperienceDialogComponent, { width: '550px' });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('the dialog was closed');
+      if (result != null)
+        this._workExperiences.push(result);
+    })
+  }
 }
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -12,6 +16,10 @@ import { MatDividerModule } from '@angular/material/divider';
     MatCardModule,
     MatListModule,
     MatDividerModule,
+    MatDialogModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: [
     MatButtonModule,
@@ -19,6 +27,10 @@ import { MatDividerModule } from '@angular/material/divider';
     MatCardModule,
     MatListModule,
     MatDividerModule,
+    MatDialogModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class SharedModule { }
