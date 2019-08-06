577a3389842ee39c38e33e331cc63db73a156ab5
Evan Smith <evanphilipsmith@gmail.com>
2019-08-06 14:43:28 -0600

    create resume component that displays work items

---
+++ cypress/integration/resume.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Resume', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:4200')
+    })
+
+    it('checks the resume component exists', () => {
+        cy.get('[data-cy=resume]')
+        cy.get('[data-cy=workExperiencesList]').children().first().as('firstExperience')
+        cy.get('@firstExperience').should('have.class', 'mat-list-item')
+    })
+})
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkExperienceComponent } from '../work-experience/work-experience.component';
+import { WorkExperiencesService } from '../services/work-experiences.service';
 
 @Component({
   selector: 'app-resume',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
+  private _workExperiences: WorkExperienceComponent[];
 
-  constructor() { }
+  constructor(_service: WorkExperiencesService) {
+    this._workExperiences = _service.getExperiences();
+  }
 
   ngOnInit() {
   }
+
+  get workExperiences() { return this._workExperiences; }
 
 }
