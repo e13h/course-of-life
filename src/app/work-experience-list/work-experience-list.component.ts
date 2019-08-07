3023f0b3434d8d9e5eb08e416c7f3579de8c4c26
Evan Smith <evanphilipsmith@gmail.com>
2019-08-07 08:35:55 -0600

    make work experience list host all the code for a work experience card

--- src/app/work-experience-list/work-experience-list.component.ts
+++ src/app/work-experience-list/work-experience-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkExperienceComponent } from '../work-experience/work-experience.component';
+import { WorkExperiencesService } from '../services/work-experiences.service';
+import { AddExperienceDialogComponent } from '../add-experience-dialog/add-experience-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-work-experience-list',
@@ -6,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-experience-list.component.scss']
 })
 export class WorkExperienceListComponent implements OnInit {
+  private _workExperiences: WorkExperienceComponent[];
+  
+  constructor(_service: WorkExperiencesService, public dialog: MatDialog) {
+    this._workExperiences = _service.getExperiences();
+  }
 
-  constructor() { }
+  get workExperiences() { return this._workExperiences; }
 
+  openDialogAddExperience(): void {
+    const dialogRef = this.dialog.open(AddExperienceDialogComponent, { width: '550px' });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('the dialog was closed');
+      if (result != null)
+        this._workExperiences.push(result);
+    })
+  }
   ngOnInit() {
   }
 
