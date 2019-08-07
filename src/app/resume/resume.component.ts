c7cbd000788f37e727ed10c1dbe9699b716c4862
Evan Smith <evanphilipsmith@gmail.com>
2019-08-07 08:37:49 -0600

    remove work experience methods and data members from resume object

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { WorkExperienceComponent } from '../work-experience/work-experience.component';
-import { WorkExperiencesService } from '../services/work-experiences.service';
-import { MatDialog } from '@angular/material/dialog';
-import { AddExperienceDialogComponent } from '../add-experience-dialog/add-experience-dialog.component';
 
 @Component({
   selector: 'app-resume',
@@ -10,24 +6,6 @@ import { AddExperienceDialogComponent } from '../add-experience-dialog/add-exper
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
-  private _workExperiences: WorkExperienceComponent[];
-
-  constructor(_service: WorkExperiencesService, public dialog: MatDialog) {
-    this._workExperiences = _service.getExperiences();
-  }
-
-  ngOnInit() {
-  }
-
-  get workExperiences() { return this._workExperiences; }
-
-  openDialogAddExperience(): void {
-    const dialogRef = this.dialog.open(AddExperienceDialogComponent, { width: '550px' });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('the dialog was closed');
-      if (result != null)
-        this._workExperiences.push(result);
-    })
-  }
+  constructor() { }
+  ngOnInit() { }
 }
