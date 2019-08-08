77ebca68e9db95a255afe442aa3f46d0ce296c98
Evan Smith <evanphilipsmith@gmail.com>
2019-08-08 15:29:30 -0600

    route landing page and resume components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResumeComponent } from './components/resume/resume.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: '', component: ResumeComponent },
+  { path: '', component: LandingPageComponent },
+  { path: 'resume', component: ResumeComponent },
 ];
 
 @NgModule({
